778a8fdc051f9797f86a88ece1c0bc51d2a77c18
NataliaVozhdaeva <lomznat@yandex.ru>
2023-06-12 02:03:37 +0700

    add: API key

--- news-JS-main/src/components/controller/appLoader.js
+++ news-JS-main/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'b97804de4e364338a2223a3c5cc78432',
         });
     }
 }

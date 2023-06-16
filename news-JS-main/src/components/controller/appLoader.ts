e4de2aceed19a2ec1a9c1490741747aaf2186c01
NataliaVozhdaeva <lomznat@yandex.ru>
2023-06-16 19:26:37 +0700

    feat: add link for deploy

--- news-JS-main/src/components/controller/appLoader.ts
+++ news-JS-main/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: 'b97804de4e364338a2223a3c5cc78432',
         });
     }

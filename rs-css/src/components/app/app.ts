193552f552749497a78a337781421c39f584aac5
NataliaVozhdaeva <lomznat@yandex.ru>
2023-07-10 21:37:35 +0700

    feat: rewrite on slasses interactivity

--- rs-css/src/components/app/app.ts
+++ rs-css/src/components/app/app.ts
@@ -25,14 +25,14 @@ export default class Game {
         this.input = document.querySelector<HTMLInputElement>('.input');
         this.okBtn = document.querySelector<HTMLElement>('.btn');
 
-        this.okBtn?.addEventListener('click', () => this.checkAnswear());
-        document.addEventListener('keyup', (event) => {
+        this.okBtn?.addEventListener('click', (): void => this.checkAnswear());
+        document.addEventListener('keyup', (event): void => {
             if (event.code === 'Enter') {
                 this.checkAnswear();
             }
         });
 
-        this.levels.forEach((el) =>
+        this.levels.forEach((el): void =>
             el.addEventListener('click', (e): void => {
                 this.changeLevel(e);
             })
@@ -45,6 +45,7 @@ export default class Game {
         this.layout.innerHTML = new LayoutView(this.layoutComponents[currentLevel]).render;
         this.htmlEditor.innerHTML = new CodeView(this.codeComponents[currentLevel]).render;
         this.taskContainer.innerHTML = new TaskView(this.taskComponents[currentLevel]).render;
+        this.interactivity();
     }
 
     checkAnswear(): void {
@@ -70,7 +71,6 @@ export default class Game {
                 this.layout.innerHTML = '';
                 this.taskContainer.innerHTML = '';
                 this.init(this.currentLevel);
-                //interactivity();
             }, 1500);
         } else {
             this.layout.querySelectorAll('.el').forEach((element): void => element.classList.add('shake'));
@@ -82,122 +82,47 @@ export default class Game {
         }
     }
 
-    changeLevel(e: Event) {
+    changeLevel(e: Event): void {
         if (!e.target) throw TypeError;
         const levelName = e.target as HTMLButtonElement;
         this.levels[this.currentLevel].classList.remove('current');
         this.currentLevel = Number(levelName.dataset.num);
         this.init(this.currentLevel);
-        //interactivity();
+    }
+
+    interactivity(): void {
+        const el = document.querySelectorAll<HTMLElement>('.el');
+        const tag = document.querySelectorAll<HTMLElement>('.tag');
+        el?.forEach((element, index): void => {
+            element.addEventListener('mouseenter', (): void => {
+                tag[index].classList.add('hover');
+
+                const popup: HTMLElement = document.createElement('div');
+                popup.className = 'popup';
+                popup.textContent = tag[index].textContent;
+                element.append(popup);
+            });
+
+            element.addEventListener('mouseleave', (): void => {
+                tag[index].classList.remove('hover');
+                document.querySelector('.popup')?.remove();
+            });
+        });
+
+        tag.forEach((element, index): void => {
+            element.addEventListener('mouseenter', (): void => {
+                el[index].classList.add('hover');
+
+                const popup: HTMLElement = document.createElement('div');
+                popup.className = 'popup';
+                popup.textContent = tag[index].textContent;
+                el[index].append(popup);
+            });
+
+            element.addEventListener('mouseleave', (): void => {
+                el[index].classList.remove('hover');
+                document.querySelector('.popup')?.remove();
+            });
+        });
     }
 }
-
-/*
-let currentLevel = 0;
-const level = document.querySelector<HTMLElement>('.level');
-const levels = document.querySelectorAll<HTMLElement>('.level-item');
-
-level?.addEventListener('click', (e): void => {
-    if (!e.target) throw TypeError;
-    const levelName = e.target as HTMLButtonElement;
-    currentLevel = Number(levelName.dataset.num);
-    init(currentLevel);
-    interactivity();
-});
-
-export function init(currentLevel: number): void {
-    const htmlEditor = document.querySelector<HTMLElement>('.html-view');
-    const taskContainer = document.querySelector<HTMLElement>('.task');
-    const layout = document.querySelector<HTMLElement>('.game-part');
-    levels[currentLevel].classList.add('current');
-
-    if (!htmlEditor || !layout || !taskContainer) throw TypeError;
-
-    htmlEditor.innerHTML = codeComponents[currentLevel];
-    layout.innerHTML = layioutComponents[currentLevel];
-     taskContainer.innerHTML = taskComponents[currentLevel];
-}
-
-export function interactivity(): void {
-    const el = document.querySelectorAll<HTMLElement>('.el');
-    const tag = document.querySelectorAll<HTMLElement>('.tag');
-    el?.forEach((element, index): void => {
-        element.addEventListener('mouseenter', (): void => {
-            tag[index].classList.add('hover');
-
-            const popup: HTMLElement = document.createElement('div');
-            popup.className = 'popup';
-            popup.textContent = tag[index].textContent;
-            element.append(popup);
-        });
-
-        element.addEventListener('mouseleave', (): void => {
-            tag[index].classList.remove('hover');
-            document.querySelector('.popup')?.remove();
-        });
-    });
-
-    tag.forEach((element, index): void => {
-        element.addEventListener('mouseenter', (): void => {
-            el[index].classList.add('hover');
-
-            const popup: HTMLElement = document.createElement('div');
-            popup.className = 'popup';
-            popup.textContent = tag[index].textContent;
-            el[index].append(popup);
-        });
-
-        element.addEventListener('mouseleave', (): void => {
-            el[index].classList.remove('hover');
-            document.querySelector('.popup')?.remove();
-        });
-    });
-}
-
-const okBtn = document.querySelector<HTMLElement>('.btn');
-okBtn?.addEventListener('click', checkAnswear);
-document.addEventListener('keyup', function (event) {
-    if (event.code === 'Enter') {
-        checkAnswear();
-    }
-});
-
-function checkAnswear(): void {
-    const el = document.querySelectorAll<HTMLElement>('.el');
-    const input = document.querySelector<HTMLInputElement>('.input');
-    const htmlEditor = document.querySelector<HTMLElement>('.html-view');
-    const layout = document.querySelector<HTMLElement>('.game-part');
-    const task = document.querySelector<HTMLElement>('.task');
-
-    if (!input) throw TypeError;
-
-    if (
-        input.value === answears[currentLevel] ||
-        input.value === answears[currentLevel][0] ||
-        input.value === answears[currentLevel][1]
-    ) {
-        input.value = '';
-        el.forEach((element) => element.classList.add('disappear'));
-        levels[currentLevel].classList.remove('current');
-        levels[currentLevel].classList.add('done');
-        currentLevel += 1;
-        levels[currentLevel].classList.add('current');
-
-        if (!htmlEditor || !layout || !task) throw TypeError;
-        setTimeout(() => {
-            htmlEditor.innerHTML = '';
-            layout.innerHTML = '';
-            task.innerHTML = '';
-            init(currentLevel);
-            interactivity();
-        }, 1500);
-    } else {
-        el.forEach((element) => {
-            element.classList.add('shake');
-            input.value = '';
-            setTimeout(() => {
-                element.classList.remove('shake');
-            }, 1500);
-        });
-    }
-} */
